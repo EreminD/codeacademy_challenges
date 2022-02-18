9a932f499f66fa11660afe2a25f678db8e6da17a
EreminD <mitya1705@gmail.com>
2022-02-18 11:26:29 +0300

    Create TopScoreSorter.js

---
+++ TopScoreSorter.js
@@ -0,0 +1,7 @@
+function scoreSorter(array, topScore) {
+  return array.sort((a,b) => b-a)
+}
+
+console.log(scoreSorter([1, 2, 3, 9999, 13], 10000))
+// Leave this so we can test your code:
+module.exports = scoreSorter;
