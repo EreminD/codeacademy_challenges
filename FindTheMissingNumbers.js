6cf8e5948e11022691bdfc1b9a1bdbe1f96dcbb0
EreminD <mitya1705@gmail.com>
2022-02-18 11:11:55 +0300

    Create FindTheMissingNumbers.js

---
+++ FindTheMissingNumbers.js
@@ -0,0 +1,12 @@
+function missingNos(array, k) {
+  const length = array[array.length-1]
+  const fullArr = [...Array(length+1).keys()].slice(1)
+
+  return fullArr.filter(x => !array.includes(x));
+}
+
+const testArray = [1, 2, 4, 5, 6, 7, 8, 10];
+console.log(missingNos(testArray, 2));
+
+// Leave this so we can test your code:
+module.exports = missingNos;
